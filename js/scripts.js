6705d1c0678a7c89c793b22f6e1169d391c76d77
Mark Atherton <mark@insightinternet.co.uk>
2017-02-18 21:29:30 +0000

    finished responsivness of index page and add toottips on image map

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,49 @@
 /* scipt to highlight link on map area hover */
 
-var aberdeen = document.getElementById("aberdeen");
-var dundee = document.getElementById("dundee");
-var edinburgh = document.getElementById("edinburgh");
-var glasgow = document.getElementById("glasgow");
+function imageMap() {
 
-aberdeen.addEventListener("mouseenter", function() {
-	document.getElementById("AberdeenLink").className = "active";
-	aberdeen.addEventListener("mouseleave", function() {
-		document.getElementById("AberdeenLink").className = " ";
-	});
-});
+	var aberdeen = document.getElementById("aberdeen");
+	var dundee = document.getElementById("dundee");
+	var edinburgh = document.getElementById("edinburgh");
+	var glasgow = document.getElementById("glasgow");
 
-dundee.addEventListener("mouseenter", function() {
-	document.getElementById("DundeeLink").className = "active";
-	dundee.addEventListener("mouseleave", function() {
-		document.getElementById("DundeeLink").className = " ";
+	aberdeen.addEventListener("mouseenter", function() {
+		document.getElementById("AberdeenLink").className = "active";
+		document.getElementById("tooltipAberdeen").className += " visable";
+		document.getElementsByClassName("highlight aberdeen").className = " ";
+		aberdeen.addEventListener("mouseleave", function() {
+			document.getElementById("tooltipAberdeen").className = "tooltip aberdeen";
+			document.getElementById("AberdeenLink").className = " ";
+		});
 	});
-});
 
-edinburgh.addEventListener("mouseenter", function() {
-	document.getElementById("EdinburghLink").className = "active";
-	edinburgh.addEventListener("mouseleave", function() {
-		document.getElementById("EdinburghLink").className = " ";
+	dundee.addEventListener("mouseenter", function() {
+		document.getElementById("DundeeLink").className = "active";
+		document.getElementById("tooltipDundee").className += " visable";
+		dundee.addEventListener("mouseleave", function() {
+			document.getElementById("tooltipDundee").className = "tooltip dundee";
+			document.getElementById("DundeeLink").className = " ";
+		});
 	});
-});
 
-glasgow.addEventListener("mouseenter", function() {
-	document.getElementById("GlasgowLink").className = "active";
-	glasgow.addEventListener("mouseleave", function() {
-		document.getElementById("GlasgowLink").className = " ";
+	edinburgh.addEventListener("mouseenter", function() {
+		document.getElementById("EdinburghLink").className = "active";
+		document.getElementById("tooltipEdinburgh").className += " visable";
+		edinburgh.addEventListener("mouseleave", function() {
+			document.getElementById("tooltipEdinburgh").className = "tooltip edinburgh";
+			document.getElementById("EdinburghLink").className = " ";
+		});
 	});
-});
+
+	glasgow.addEventListener("mouseenter", function() {
+		document.getElementById("GlasgowLink").className = "active";
+		document.getElementById("tooltipGlasgow").className += " visable";
+		glasgow.addEventListener("mouseleave", function() {
+			document.getElementById("tooltipGlasgow").className = "tooltip glasgow";
+			document.getElementById("GlasgowLink").className = " ";
+		});
+	});
+	
+}
+
+imageMap();
